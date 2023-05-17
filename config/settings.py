566baf7762807814f5658b232e051c90c0f25ea7
lienero <lienero@naver.com>
2023-05-17 16:07:32 +0900

    모델 작성(db작성)

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'yangpt_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- yangpt_app/models.py
+++ yangpt_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField('닉네임', max_length=50)
+    ai_liked = models.IntegerField('ai의 호감도', default=0)
+    common_event_flag = models.IntegerField('공통 이벤트 플래그', default=0)
+    yan_event_flag = models.IntegerField('YAN 이벤트 플래그', default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name
+
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField('메시지', max_length=255)
+    from_ai = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'chats'
+
+    def __str__(self):
+        return self.content
